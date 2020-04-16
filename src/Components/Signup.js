5413c7f6c5c428c513362a3195e00bbe16fc30fc
Julie Brosse <privatejuliedev@gmail.com>
2020-04-16 16:44:12 +0200

    exercice finished

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,21 +1,75 @@
 import React, {Component, Fragment} from 'react';
 import './Signup.css'
 
+
+const thisState = {
+  email: '',
+  password: '',
+  passwordConfirmed: false,
+  name: '',
+  lastname: ''
+}
+
 export default class Signup extends Component {
-  state = {email: 'hello@gmail.com'};
+  state = thisState
 
+  updateEmailField =(e) => {
+    this.setState ({
+      email: e.target.value
+    })
+  }
 
-  updateEmailField = (event) => {
-    this.setState({
-      email: event.target.value
-    });
+  updatePasswordField = (e) => {
+    this.setState ({
+      password: e.target.value
+    })
+  }
+
+  checkPasswordConfirmation = (e) => {
+    if (e.target.value === this.state.password) {
+      this.setState ({
+        passwordConfirmed: true
+      })
+    }
+  }
+
+  updateNameField = (e) => {
+    this.setState ({
+      name: e.target.value
+    })
+  }
+
+  updateLastNameField = (e) => {
+    this.setState ({
+      lastname: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    console.log(this.state)
+    if (this.state.passwordConfirmed === true ) {
+      console.log('Password confirmed')
+    } else {
+      console.log('Needs password confirmation')
+    }
   }
 
   render() {
     return (
         <Fragment>
-        <h1>{this.state.email}</h1>
-        <input type="email" onChange={this.updateEmailField} />
+          <h1>email : {this.state.email }</h1>
+          <h1>name : {this.state.name }</h1>
+          <h1>last name :{this.state.lastname }</h1>
+
+        
+        <form onClick={this.handleSubmit}>
+          <input type="email" placeholder="email" onChange={this.updateEmailField} />
+          <input type="password" placeholder="password" onChange={this.updatePasswordField} />
+          <input type="password" placeholder="password confirmation" onChange={this.checkPasswordConfirmation} />
+          <input type="text" placeholder="first name" onChange={this.updateNameField} />
+          <input type="text" placeholder="last name" onChange={this.updateLastNameField} />
+          <input type="submit" value="Submit" />
+        </form>
       </Fragment>
     );
   }
@@ -45,4 +99,82 @@ export default class Signup extends Component {
        
 //     }
 // }
+
+
+
+// const initialState = {
+//   user : {
+//     id: 0,
+//     email: '',
+//     password: '',
+//     name: '',
+//     lastname: ''
+//   }
+// }
+
+// class Signup extends Component {
+//   state = initialState
+
+//   updateEmailField = (event) => {
+//     console.log(event.target.value)
+//     this.setState({
+//       email: event.target.value
+//     });
+//     console.log('email is' + this.state.email)
+//   }
+//   updatePasswordField = (event) => {
+//     this.setState({
+//       password: event.target.value
+//     });
+//     console.log(this.state.password)
+
+//   }  
+//   updateNameField = (event) => {
+//     this.setState({
+//       name: event.target.value
+//     });
+//   }  
+//   updateLastNameField = (event) => {
+//     this.setState({
+//       lastname: event.target.value
+//     });
+//   }
+
+  
+
+
+//   render() {
+//     return (
+//         <Fragment>
+
+// <form onSubmit={handleSubmit}>
+//     <div>
+//       <Field
+//         component="input"
+//         name="search"
+//         onChange={handleChange}
+//         type="text"
+//         value={value}
+//       />
+//     </div>
+//   </form>
+//         <h1>email : {this.state.email}</h1>
+//         <h1>password : {this.state.password}</h1>
+//         <h1>name: {this.state.name}</h1>
+//         <h1>lastname : {this.state.lastname}</h1>
+
+//         {/* <form onSubmit={this.handleSubmit}>
+//           <input type="email" onChange={this.updateEmailField} />
+//           <input type="text" onChange={this.updatePasswordField} />
+//           <input type="text" onChange={this.updateNameField} />
+//           <input type="text" onChange={this.updateLastNameField} />
+//           <input type="submit" value="Submit" />
+//         </form> */}
+//       </Fragment>
+//     );
+//   }
+// }
+
+
+// export default Signup;
 
